c2ab40c76ce01777fd782538c1b77af891a8328d
Kristen <Kristen@DESKTOP-561CB7N>
2018-01-25 16:33:03 +0100

    include 2

---
+++ lib/php/footer.php
@@ -0,0 +1,7 @@
+  <div id="footer"> <a href="http://www.free-css.com/">Home</a> | <a href="http://www.free-css.com/">Archives</a> | <a href="http://www.free-css.com/">Apparatus</a> | <a href="http://www.free-css.com/">Members</a> | <a href="http://www.free-css.com/">Gallery</a> | <a href="http://www.free-css.com/">Training</a> | <a href="http://www.free-css.com/">Store</a> | <a href="http://www.free-css.com/">Links</a><br />
+    <br />
+    <p>&copy; Fire Rescue Squad 2006. All rights reserved. | designed by <a href="http://www.freewebsitetemplates.com">free website templates</a></p>
+ </div>
+</div>
+</body>
+</html>
