aa827a92670ee95acdc0100d1cbd9d5cd581e029
Kristen <Kristen@DESKTOP-561CB7N>
2018-02-01 16:55:36 +0100

    b

--- login.php
+++ login.php
@@ -7,13 +7,13 @@
         <form action="lib/php/SQL/connectLogin.php" method="get">
         
         	<p>
-            	<label for="login" >Login : </label>
-        		<input id="login" name="login" type="text" value="" size="10" maxlength="8" />
+            	<label for="loginName" >Login : </label>
+        		<input id="loginName" name="loginName" type="text" value="" size="10" maxlength="8" />
         		
         		<br />
         		
-        		<label for="mdp" >Mot de Passe : </label>
-        		<input id="mdp" name="mdp" type="password" value="" size="10" maxlength="8" />    
+        		<label for="loginMdp" >Mot de Passe : </label>
+        		<input id="loginMdp" name="loginMdp" type="password" value="" size="10" maxlength="8" />    
         	</p>
         	
         </form>
